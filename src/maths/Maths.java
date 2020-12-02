702d0d70866f91c734e2c1d3af79b042ec691e43
janwdev <jannik.weisser@hs-furtwangen.de>
2020-12-02 19:09:29 +0100

    Drehmatrix, Vektordrehung

--- src/maths/Maths.java
+++ src/maths/Maths.java
@@ -9,58 +9,70 @@ public class Maths {
 	private ProjektionsMatrix projektionsMatrix = new ProjektionsMatrix();
 	private Vektorberechnung bVektor = new Vektorberechnung();
 	private VektorMatrixBerechnung bVektorMatrix = new VektorMatrixBerechnung();
-	
+
 	public Matrix getProjektionsMatrix() {
 		return projektionsMatrix.getProMatrix();
 	}
+
 	public double[][] getProjektionsMatrixArray() {
 		return projektionsMatrix.getProjektionsMatrix();
 	}
-	
+
 	public Matrix multipy(Matrix ma1, Matrix ma2) {
 		return bMatrix.multipy(ma1, ma2);
 	}
-	
+
 	public double skalarProdukt(Vector u, Vector v) {
 		return bVektor.skalarProdukt(u, v);
 	}
+
 	public double VektorLaenge(Vector v) {
 		return bVektor.vecLength(v);
 	}
+
 	public double getWinkelZwischen(Vector u, Vector v) {
 		return bVektor.winkelBerechnen(u, v);
 	}
-	//TODO Vektor um Drehmatrix drehen
+
+	public Vector rotateVector(Vector v, int winkelX, int winkelY, int winkelZ) throws Exception {
+		Vector vec = bVektor.drehen(v, bMatrix.getDrehMatrix(winkelX, winkelY, winkelZ));
+		return vec;
+	}
+
 	public Vector vektorDivision(Vector v, double wert) {
 		return bVektor.vecDiv(v, wert);
 	}
+
 	public Vector vektorMultiplikation(Vector v, double wert) {
 		return bVektor.vecMulti(v, wert);
 	}
+
 	public Vector vectorAddition(Vector v, Vector u) {
 		return bVektor.vecAddition(v, u);
 	}
+
 	public Vector kreuzprodukt(Vector u, Vector v) {
 		return bVektor.kreuzprodukt(u, v);
 	}
+
 	public double getAbstand(Vector u, Vector v) {
 		return bVektor.abstandVector(u, v);
 	}
-	
+
 	public Vector multiply(Vector v, Matrix m) throws Exception {
 		return bVektorMatrix.multiply(v, m);
 	}
-	
-	public int[] vektor2DToDisplayKoordinates(Vector v){
-		//TODO auf 2D Vektor pruefen
+
+	public int[] vektor2DToDisplayKoordinates(Vector v) {
+		// TODO auf 2D Vektor pruefen
 		int[] ret;
 		double x = v.getVectorX();
 		double y = v.getVectorY();
-		x = Constants.xOffsetDraw + x*100*Constants.drawScaleFactor;
-		y = Constants.yOffsetDraw + y*100*Constants.drawScaleFactor;
+		x = Constants.xOffsetDraw + x * 100 * Constants.drawScaleFactor;
+		y = Constants.yOffsetDraw + y * 100 * Constants.drawScaleFactor;
 		int xDisp = (int) Math.round(x);
 		int yDisp = (int) Math.round(y);
-		ret = new int[]{xDisp, yDisp};
+		ret = new int[] { xDisp, yDisp };
 		return ret;
 	}
 }
--- src/maths/MatrixBerechnung.java
+++ src/maths/MatrixBerechnung.java
@@ -51,4 +51,16 @@ public class MatrixBerechnung {
 		}
 	}
 
+	public Matrix getDrehMatrix(double winkelX, double winkelY, double winkelZ) throws Exception {
+		Matrix xMatrix = new Matrix(3, 3);
+		xMatrix.init(1, 0, 0, 0, Math.cos(winkelX), -Math.sin(winkelX), 0, Math.sin(winkelX), Math.cos(winkelX));
+		Matrix yMatrix = new Matrix(3, 3);
+		yMatrix.init(Math.cos(winkelY), 0, Math.sin(winkelY), 0, 1, 0, -Math.sin(winkelY), 0, Math.cos(winkelY));
+		Matrix zMatrix = new Matrix(3, 3);
+		zMatrix.init(Math.cos(winkelZ), -Math.sin(winkelZ), 0, Math.sin(winkelZ), Math.cos(winkelZ), 0, 0, 0, 1);
+		Matrix retMa = multipy(xMatrix, yMatrix);
+		retMa = multipy(retMa, zMatrix);
+		return retMa;
+	}
+
 }
