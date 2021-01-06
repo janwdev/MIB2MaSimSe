ad4ae5a561c29b382e494ba5b7c23d1fe164a444
janwdev <jannik.weisser@hs-furtwangen.de>
2021-01-06 21:31:07 +0100

    Versionsnummer angepasst, Readme aktualisiert

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 - [Download Releases](https://github.com/janwdev/MIB2MaSimSe/releases)
 - [JavaDoc public](https://janwdev.github.io/MIB2MaSimSe/)
 
-##Description
+## Description
 Program to visualize the flight routes between two positions on the earth.
 Programmed in Java from me and [Luca](https://github.com/TutiEgg)
--- src/app/Constants.java
+++ src/app/Constants.java
@@ -10,7 +10,7 @@ import java.awt.Color;
  */
 public class Constants {
 	public static final String PROGNAME = "3DFlightMaSim - Luca Jannik";
-	public static final String VERSION = "0.001";
+	public static final String VERSION = "0.05";
 	public static final String DESCRIPTION = "Program to visualize the flight routes between two positions on the earth";
 	public static String LICENCETEXT;
 
